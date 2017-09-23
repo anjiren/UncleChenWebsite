8a9b1ec432216137859eea1f39c62bcf8c4ac7a6
Anji <renalele@gmail.com>
2017-09-23 15:15:22 -0700

    add product images

--- src/client/app/Products.jsx
+++ src/client/app/Products.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 // Components
 import MainNav from './MainNav.jsx';
 import ProductsGrid from './ProductsGrid.jsx';
+
 export default class Products extends React.Component {
   render() {
     return (
--- src/client/app/Recipes.jsx
+++ src/client/app/Recipes.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 // Components
 import MainNav from './MainNav.jsx';
+import RecipesGrid from './RecipesGrid.jsx';
 
 export default class Products extends React.Component {
   render() {
     return (
       <div>
         <MainNav/ >
+        <div className="uc-workspace">
+          <RecipesGrid />
+        </div>
       </div>
     )
   }
