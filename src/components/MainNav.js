5d5c0a1f36da1ea0b3e4c754a5df9c1c3eb0d2e6
Anji <renalele@gmail.com>
2018-10-20 19:18:34 -0700

    Update logo

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -5,7 +5,7 @@ export class MainNav extends React.Component {
   render() {
     return (
       <nav className="uc-main-nav">
-        <Link to="/"><img className="uc-main-nav__logo" type="image/png" src="/img/lh_logo_xsm.png"/></Link>
+        <Link to="/"><img className="uc-main-nav__logo" type="image/png" src="/img/union_food_logo.png"/></Link>
         <ul className="uc-main-nav__items">
           <li><h6><Link to="/products">Products</Link></h6></li>
           <li><h6><Link to="/recipes">Recipes</Link></h6></li>
